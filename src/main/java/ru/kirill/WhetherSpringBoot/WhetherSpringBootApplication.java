93609c8acd1d64df73e986bbb212ed3856974c72
kirushakrutoi <kirillpadurin@gmail.com>
2024-02-28 08:54:08 +0300

    create repo

---
+++ src/main/java/ru/kirill/WhetherSpringBoot/WhetherSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill.WhetherSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhetherSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhetherSpringBootApplication.class, args);
+	}
+
+}
